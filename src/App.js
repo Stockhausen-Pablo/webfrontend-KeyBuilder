c0698c4b248d5fab1d3e5a248c8affaec7a065a8
Pablo Stockhausen <stockhausen017@gmail.com>
2021-02-13 20:15:20 +0100

    base structure

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import {createBrowserHistory} from 'history';
+import {Router} from 'react-router-dom';
+import Routes from './Routes';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootswatch/dist/slate/bootstrap.min.css';
+
+import './App.css';
+
+const browserHistory = createBrowserHistory();
+
+
+export default class App extends Component {
+  render(){
+    return(
+        <Router history={browserHistory}>
+          <Routes/>
+        </Router>
+    )
+  }
+};
