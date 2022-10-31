4f82af3548d5b20aeb071cd3c17c99fe1a28921d
Pablo Stockhausen <stockhausen017@gmail.com>
2022-10-31 17:38:07 +0000

    feature/keyboard_component_service

--- src/App.js
+++ src/App.js
@@ -4,19 +4,36 @@ import {ThemeProvider} from '@material-ui/core/styles';
 import {Router} from 'react-router-dom';
 import Routes from './Routes';
 import theme from './theme';
+import {ApolloProvider} from 'react-apollo';
+import {ApolloClient, InMemoryCache} from 'apollo-boost';
+import {createHttpLink} from 'apollo-link-http';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const browserHistory = createBrowserHistory();
+
+const link = createHttpLink(
+  {
+    uri: process.env.REACT_APP_API_URI,
+  }
+);
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+});
+
 
 
 export default class App extends Component {
   render(){
     return(
       <ThemeProvider theme={theme}>
-        <Router history={browserHistory}>
-          <Routes/>
-        </Router>
+        <ApolloProvider client={client}>
+          <Router history={browserHistory}>
+            <Routes/>
+          </Router>
+        </ApolloProvider>
       </ThemeProvider>
     )
   }
